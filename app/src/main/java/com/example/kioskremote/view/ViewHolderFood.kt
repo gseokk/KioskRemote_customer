cee2dc5e06a2680d9025f2dd126990eaaee77aba
todd.zz <todd.zz@kakaopaycorp.com>
2020-06-22 01:35:26 +0900

    메뉴 설명 Activity(메뉴판) 안에 추가

--- app/src/main/java/com/example/kioskremote/view/ViewHolderFood.kt
+++ app/src/main/java/com/example/kioskremote/view/ViewHolderFood.kt
@@ -10,17 +10,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.kioskremote.R
 import com.example.kioskremote.dto.FoodData
 
-class ViewHolderMovie(itemView: View) :
+class ViewHolderFood(itemView: View) :
     RecyclerView.ViewHolder(itemView) {
-    var tv_movie_title: TextView
-    var iv_movie: ImageView
-    var iv_movie2: ImageView
+    var tv_food_title: TextView
+    var iv_food: ImageView
+    var iv_food2: ImageView
+    var tv_description: TextView
     var linearlayout: LinearLayout
     var onViewHolderItemClickListener: OnViewHolderItemClickListener? = null
     fun onBind(data: FoodData, position: Int, selectedItems: SparseBooleanArray) {
-        tv_movie_title.setText(data.title)
-        iv_movie.setImageResource(data.image)
-        iv_movie2.setImageResource(data.image)
+        tv_food_title.setText(data.title)
+        iv_food.setImageResource(data.image)
+        iv_food2.setImageResource(data.image)
+        tv_description.setText(data.description)
         changeVisibility(selectedItems[position])
     }
 
@@ -31,28 +33,33 @@ class ViewHolderMovie(itemView: View) :
         // Animation이 실행되는 시간, n/1000초
         va.duration = 500
         va.addUpdateListener { animation -> // imageView의 높이 변경
-            iv_movie2.layoutParams.height = animation.animatedValue as Int
-            iv_movie2.requestLayout()
+            iv_food2.layoutParams.height = animation.animatedValue as Int
+            tv_description.layoutParams.height = animation.animatedValue as Int - 300
+            iv_food2.requestLayout()
+            tv_description.requestLayout()
+
             // imageView가 실제로 사라지게하는 부분
-            iv_movie2.visibility = if (isExpanded) View.VISIBLE else View.GONE
+            iv_food2.visibility = if (isExpanded) View.VISIBLE else View.GONE
+            tv_description.visibility = if (isExpanded) View.VISIBLE else View.GONE
         }
         // Animation start
         va.start()
     }
 
     init {
-        iv_movie = itemView.findViewById(R.id.iv_movie)
-        tv_movie_title = itemView.findViewById(R.id.tv_movie_title)
-        iv_movie2 = itemView.findViewById(R.id.iv_movie2)
+        iv_food = itemView.findViewById(R.id.iv_food)
+        tv_food_title = itemView.findViewById(R.id.tv_food_title)
+        iv_food2 = itemView.findViewById(R.id.iv_food2)
+        tv_description = itemView.findViewById(R.id.description)
         linearlayout = itemView.findViewById(R.id.linearlayout)
         linearlayout.setOnClickListener { onViewHolderItemClickListener!!.onViewHolderItemClick() }
     }
 
     companion object {
         fun setOnViewHolderItemClickListener(
-            viewHolderMovie: ViewHolderMovie,
+            viewHolderFood: ViewHolderFood,
             onViewHolderItemClickListener: OnViewHolderItemClickListener?) {
-            viewHolderMovie.onViewHolderItemClickListener = onViewHolderItemClickListener
+            viewHolderFood.onViewHolderItemClickListener = onViewHolderItemClickListener
         }
     }
 }
